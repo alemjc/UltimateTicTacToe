4c8f4520ce936b2280aa22318da701fda1f0b761
alemjc <alemjc2204@gmail.com>
2015-11-07 00:39:53 -0500

    Refactor class names. no need for big table index. changed the design of the table, I don't know how it looks right now, but it is programmed how it was discussed. I further implemented the updateTable method this is also to check if user made a valid move, then to create the temporary table.

--- app/src/main/java/com/games/ultimatetictactoe/app/TableIndex.java
+++ app/src/main/java/com/games/ultimatetictactoe/app/TableIndex.java
@@ -5,24 +5,24 @@ package com.games.ultimatetictactoe.app;
  */
 public class TableIndex implements Index {
 
-    private int tableNumber;
+    /*private int tableNumber;*/ //Doesn't seem like we need a tableNumber
     private STATE state;
 
-    public TableIndex(int tableNumber, STATE state){
-        this.tableNumber = tableNumber;
+    public TableIndex(/*int tableNumber,*/ STATE state){
+        //this.tableNumber = tableNumber;
         this.state = state;
     }
     public STATE getState(){
         return state;
     }
 
-    public int getTableNumber(){
+    /*public int getTableNumber(){
         return tableNumber;
-    }
+    }*/
 
-    public void setTableNumber(int tableNumber){
+    /*public void setTableNumber(int tableNumber){
         this.tableNumber = tableNumber;
-    }
+    }*/
 
     public void setState(STATE state){
         this.state = state;
